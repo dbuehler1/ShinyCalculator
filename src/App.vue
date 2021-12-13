30af3819af1899993f9adba80c809e17d412a9f7
Daniel <dbuehler1@my.wctc.edu>
2021-12-13 01:12:26 -0600

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="border border-dark rounded p-2 bg-secondary">
+      <h2 class="bg-info p-2 border border-dark">Shiny Calculator</h2>
+      <h3>Your Number: 1576</h3>
+      <h5>Number of Guesses: {{this.counter}}</h5>
+      <ShinyNumList
+          :shiny-collection="numbers"
+          :test-num="this.testNum"
+      ></ShinyNumList><br>
+      <button class="btn btn-success float-right" @click="calcShiny">Find Shiny</button><br><br>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShinyNumList from "@/components/ShinyNumList";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      numbers : [],
+      counter : 0,
+      testNum : 0,
+    }
+  },
   components: {
-    HelloWorld
+    ShinyNumList
+
+  },
+  methods : {
+    calcShiny() {
+      var testnum = 0;
+      var originalNum = 1576;
+      this.counter = 0;
+
+      this.numbers.splice(0);
+      do {
+        testnum = Math.floor(Math.random() * 4096) + 1;
+        //8192
+        this.numbers.push(testnum);
+        this.counter++;
+      } while (testnum !== originalNum);
+      this.testNum = testnum;
+
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+  margin-right: auto;
+  margin-left: auto;
+  width: 20rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 1rem;
 }
 </style>
