877b0e3d7cbdc1465e4114723091c48b12c0a995
Daniel <dbuehler1@my.wctc.edu>
2021-12-13 01:14:40 -0600

    test

---
+++ src/components/ShinyNumList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="shiny border border-dark">
+    <ShinyNumListItem  v-for="(num, i) in shinyCollection"
+                      :key="i"
+                      :num="num"
+                       :testnum="testNum"
+    ></ShinyNumListItem>
+  </div>
+</template>
+
+<script>
+import ShinyNumListItem from "@/components/ShinyNumListItem";
+export default {
+name: "ShinyNumList",
+  props : {
+    shinyCollection : Array,
+    testNum : Number,
+  },
+  components: {ShinyNumListItem}
+}
+</script>
+
+<style scoped>
+.shiny {
+  background-color: lightgray;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  height: 20rem;
+  width: 15rem;
+  overflow: auto;
+}
+</style>
---
+++ src/components/ShinyNumListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-bind:class="{'bg-success text-light border border-dark': num === testnum}">
+    {{this.num}}
+  </div>
+</template>
+
+<script>
+export default {
+name: "ShinyNumListItem",
+  props : {
+    num : Number,
+    testnum : Number,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
